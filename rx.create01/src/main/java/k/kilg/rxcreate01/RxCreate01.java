e2cfaa40e2564f6530b6b6f27fe5c31d29e4f4e6
ki16or@gmail.com <ki16or@gmail.com>
2018-11-08 01:42:16 +0500

    Observer.create() done

--- rx.create01/src/main/java/k/kilg/rxcreate01/RxCreate01.java
+++ rx.create01/src/main/java/k/kilg/rxcreate01/RxCreate01.java
@@ -1,22 +1,39 @@
 package k.kilg.rxcreate01;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.reactivex.Observable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observables.ConnectableObservable;
 
 @SuppressLint("CheckResult")
 public class RxCreate01 extends AppCompatActivity {
 
     @BindView(R.id.text)
-    private TextView text;
+    public TextView text;
+
+    private static final String[] LOCATION_PERMS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
+     CompositeDisposable subscription = new CompositeDisposable();
 
 
     @Override
@@ -25,19 +42,22 @@ public class RxCreate01 extends AppCompatActivity {
         setContentView(R.layout.activity_rx_create01);
 
         ButterKnife.bind(this);
+    }
 
-
-
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        subscription.clear();
     }
 
     @OnClick(R.id.btn01Simple)
-    private void clickSimple() {
+    public void clickSimple() {
+        clearText();
         /**
          * В subscriber размещается Consumer c "accept" callback.
          * в случае onComplete там размещается Action.
          */
-        RxUtils.getIntObserver(7)
+        RxUtils.getObserver(7)
                 .subscribe(
                         // onNext
                         this::showText,
@@ -49,9 +69,9 @@ public class RxCreate01 extends AppCompatActivity {
     }
 
     @OnClick(R.id.btn02WithCache)
-    private void clickWithCache() {
-
-        RxUtils.getIntObserverWithCache(4, 5)
+    public void clickWithCache() {
+        clearText();
+        RxUtils.getObserverWithCache(4, 5)
                 .subscribe(
                         this::showText,
                         error -> showError(error.getMessage()),
@@ -59,22 +79,119 @@ public class RxCreate01 extends AppCompatActivity {
                 );
 
     }
-
 
     @OnClick(R.id.btn03WithUnSubscribe)
-    private void click03() {
-
-        RxUtils.getIntObserverWithUnSubscriber(1, 7)
+    public void click03() {
+        clearText();
+        Disposable disposable = RxUtils.getObserverWithUnSubscriber(1, 7)
                 .subscribe(
                         this::showText,
                         error -> showError(error.getMessage()),
                         this::showCompleteStatus
                 );
+        subscription.add(disposable);
 
     }
+
+    @OnClick(R.id.btn04Error)
+    public void click04() {
+        clearText();
+        RxUtils.getErrorPattern()
+                .subscribe(
+                        this::showText,
+                        error -> showError(error.getMessage()),
+                        this::showCompleteStatus
+                );
+    }
+
+    @OnClick(R.id.btn05HotObserver)
+    public void click05() {
+        checkGpsPermissions();
+    }
+
+    private void checkGpsPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(LOCATION_PERMS, 1);
+            }
+
+        } else {
+            loadHotObserver2();
+        }
+    }
+
+    private void loadHotObserver() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        RxUtils.getHotObservable(locationManager)
+                .subscribe(location -> {
+                    text.setText(String.valueOf(location.getLatitude() + " " + location.getLongitude()));
+                },
+                        error -> showError(error.getMessage()),
+                        this::showCompleteStatus);
+    }
+
+    /**
+     * .publish().refCount() подсчитывают количество активных подписчиков.
+     * Когда число подписчиков изменяется с 0 до 1 - производится подписка.
+     * Все другие подписки игнорируются, и один и тот же обернутый Subscriber
+     * совместно используется всеми подписчиками.
+     * Когда подписчиков становится 0 - идет отписка.
+     * Таким образом, используется одно соединение к какому-то ни было ресурсу.
+     */
+    private void loadHotObserver2() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Observable<Location> lazy = RxUtils.getHotObservable(locationManager).publish().refCount();
+        System.out.println(">>>> До всех подписчиков");
+        Disposable s1 = lazy.subscribe();
+        System.out.println(">>>> 1 подписка");
+        Disposable s2 = lazy.subscribe();
+        System.out.println(">>>> 2 подписка");
+        s1.dispose();
+        System.out.println(">>>> 1 отписка");
+        s2.dispose();
+        System.out.println(">>>> 2 отписка");
+    }
+
+    /**
+     * Вызывая у Observable метод publish()
+     * получаем #ConnectableObservable.
+     * Впринципе можно и дальше пользоваться этим Observable,
+     * но любой клиент подписавшийся на #ConnectableObservable запоминается.
+     * Пока не вызван метод #connect ни один из клиентов не подписан на изначальный Observable.
+     * После #connect подписчик-посредник подписывается на Observable,
+     * несмотря на количество клиентов, пусть их даже и нет.
+     * Если в множестве есть подписчики,
+     * то они будут получать одну и ту же последовательность.
+     */
+    private void loadHotObserver3() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        ConnectableObservable<Location> published = RxUtils.getHotObservable(locationManager).publish();
+        Disposable d1 = published.subscribe();
+        Disposable d2 = published.subscribe();
+        published.connect();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    loadHotObserver();
+                }
+            }
+
+        }
+    }
+
 
     private void showText(Integer integer) {
         text.setText(text.getText().toString().concat(integer.toString()));
+    }
+
+    private void clearText() {
+        text.setText("");
     }
 
     private void showCompleteStatus() {
@@ -82,6 +199,6 @@ public class RxCreate01 extends AppCompatActivity {
     }
 
     private void showError(String message) {
-
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
--- rx.create01/src/main/java/k/kilg/rxcreate01/RxUtils.java
+++ rx.create01/src/main/java/k/kilg/rxcreate01/RxUtils.java
@@ -1,11 +1,18 @@
 package k.kilg.rxcreate01;
 
+import android.annotation.SuppressLint;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.functions.Cancellable;
+import io.reactivex.subjects.PublishSubject;
 
 public class RxUtils {
 
@@ -16,7 +23,7 @@ public class RxUtils {
      * @param id некое число
      * @return Observable возвращает заданный id
      */
-    public static Observable<Integer> getIntObserver(int id) {
+    public static Observable<Integer> getObserver(int id) {
 
         return Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
@@ -28,7 +35,7 @@ public class RxUtils {
     }
 
     // lambda version
-    public static Observable<Integer> getIntObserverLambda(int id) {
+    public static Observable<Integer> getObserverLambda(int id) {
 
         return Observable.create(emitter -> {
             emitter.onNext(id);
@@ -46,7 +53,7 @@ public class RxUtils {
      * @param range некий интервал
      * @return
      */
-    public static Observable<Integer> getIntObserverWithCache(final int id, int range) {
+    public static Observable<Integer> getObserverWithCache(final int id, int range) {
         return Observable.<Integer>create(emitter -> {
             if (range <= 0) {
                 emitter.onError(new Throwable("Range must be positive and non zero."));
@@ -60,9 +67,9 @@ public class RxUtils {
 
     /**
      * 03. Обработка отписки
-     * примитивная версия
+     * примитивная версия, работает с бесконечными потоками
      */
-    public static Observable<Integer> getIntObserverWithUnSubscriber(final int id, int range) {
+    public static Observable<Integer> getObserverWithUnSubscriber(final int id, int range) {
         return Observable.<Integer>create(emitter -> {
 
             if (range <= 0) {
@@ -71,13 +78,15 @@ public class RxUtils {
 
             Runnable r = () -> {
                 for (int index = id; index < id + range; index++) {
-                    Utils.sleep(10, TimeUnit.SECONDS);
+                    Utils.sleep(2, TimeUnit.SECONDS);
                     if (!emitter.isDisposed()) {
                         emitter.onNext(index);
                     } else {
+                        Log.d("###", ">>> Unsubscribe!");
                         emitter.onComplete();
                     }
                 }
+                emitter.onComplete();
             };
             final Thread thread = new Thread(r);
             thread.start();
@@ -85,25 +94,128 @@ public class RxUtils {
             /**
              * Здесь установлен листенер на отписку.
              * Отписка -> поток прерывается -> onComplete
+             * Но кажется что это нихрена не работает.
              */
-            emitter.setCancellable(thread::interrupt);
+            emitter.setCancellable(() -> {
+                Log.d("###", ">>> Interrupt!");
+                thread.interrupt();
+            });
 
         });
     }
 
-
-
-
-
-
-
-
-    public static Observable<Integer> test() {
-        return Observable.<Integer>create(new ObservableOnSubscribe<Integer>() {
-            @Override
-            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
-                emitter.onComplete();
-            }
+    /**
+     * 04. Обработка ошибок.
+     * Рекомендация: заключать целые выражения внутри create() в try-catch.
+     *
+     * @param index
+     * @return
+     */
+    public static Observable<Integer> getErrorObserver(int index) {
+        return Observable.create(emitter -> {
+            if (index == 0) emitter.onError(new Throwable("Errors always go!"));
+            emitter.onNext(index);
+            emitter.onComplete();
         });
+    }
+
+    /**
+     * fromCallable отрабатывает паттерн с обертыванием в try-catch
+     * и возвращением одного значения.
+     * внутрь кладется метод, получающие либо одно значение, либо ошибку
+     *
+     * @return либо ошибка, либо число.
+     */
+    public static Observable<Integer> getErrorPattern() {
+        return Observable.fromCallable(() -> {
+            return loadPossibleError(0);
+        });
+    }
+
+    private static Integer loadPossibleError(int index) throws Exception {
+        if (index == 0) throw new Exception("This error is fromCallable");
+        return 42;
+    }
+
+    /**
+     * 05. пример создания горячего Observable.
+     * когда есть некий листенер и он асинхронно колбэками выдает какую-то информацию.
+     *
+     * @param locationManager
+     * @return
+     */
+    @SuppressLint("MissingPermission")
+    public static Observable<Location> getHotObservable(LocationManager locationManager) {
+        return Observable.create(emitter -> {
+            System.out.println(">>> Открывается соединение");
+            LocationListener locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    // новая порция информации
+                    emitter.onNext(location);
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+                    //когда провайдеров не останется - поток завершается.
+                    emitter.onComplete();
+                }
+            };
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        });
+    }
+
+
+    /**
+     *  06. Subject
+     *  Расширяет Observable и одновременно реализует Observer.
+     *  Полезное средство создания экземпляров Observable в случае,
+     *  когда Observable.create() кажется слишком сложным.
+     *  Есть разные ньюансы, поэтому лучше вначале пробовать Observable.create()
+     *  и фабричные методы.
+     * @param locationManager
+     * @return
+     */
+    @SuppressLint("MissingPermission")
+    public static Observable<Location> getHotObservableThroughSubject(LocationManager locationManager) {
+
+        final PublishSubject<Location> subject = PublishSubject.create();
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // новая порция информации
+                subject.onNext(location);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                subject.onComplete();
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        return subject;
+
     }
 }
