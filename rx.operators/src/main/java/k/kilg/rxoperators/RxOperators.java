7c803729d16f261632150b2f2617f5e0dfb44745
ki16or@gmail.com <ki16or@gmail.com>
2018-12-20 01:03:31 +0500

    Observer.operators

---
+++ rx.operators/src/main/java/k/kilg/rxoperators/RxOperators.java
@@ -0,0 +1,13 @@
+package k.kilg.rxoperators;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RxOperators extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx_operators);
+    }
+}
